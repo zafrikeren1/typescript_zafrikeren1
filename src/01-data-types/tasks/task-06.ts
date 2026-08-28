f493326fa236105d43a8e217bd55edb73d5c83c0
zafrikeren1 <zafrimaheswara21@gmail.com>
2026-08-28 11:15:01 +0700

    datatype 06-07

--- src/01-data-types/tasks/task-06.ts
+++ src/01-data-types/tasks/task-06.ts
@@ -9,3 +9,47 @@
  * 
  * display the product data using console.log.
  */
+
+type Product = {
+    productCode: string; 
+    productName: string;
+    sellingPrice: number;
+    stockQuantity: number;
+    productWeight: number;
+    averageCustomerRating: number;
+    isDiscounted: boolean;
+};
+
+const product1: Product = {
+    productCode: "PRD001",
+    productName: "Wireless Mouse",
+    sellingPrice: 150000,
+    stockQuantity: 50,
+    productWeight: 0.2,
+    averageCustomerRating: 4.5,
+    isDiscounted: true,
+};
+
+const product2: Product = {
+    productCode: "PRD002",
+    productName: "Gaming Keyboard",
+    sellingPrice: 300000,
+    stockQuantity: 30,
+    productWeight: 1.2,
+    averageCustomerRating: 4.7,
+    isDiscounted: false,
+};
+
+const product3 : Product = {
+    productCode: "PRD003",
+    productName: "Gaming Mouse",
+    sellingPrice: 250000,
+    stockQuantity: 42,
+    productWeight: 0.8,
+    averageCustomerRating: 4.5,
+    isDiscounted: true,
+}
+
+console.log("Product 1 : ", product1)
+console.log("Prpduct 2 : ", product2)
+console.log("product 3 : ", product3)
--- src/01-data-types/tasks/task-07.ts
+++ src/01-data-types/tasks/task-07.ts
@@ -11,3 +11,58 @@
  * 
  * display the registration data using console.log.
  */
+type CourseRegistration = {
+    StudentId: number;
+    StudentName: string;
+    GradeLvl: number;
+
+    CourseId: number;
+    CourseTitle: string;
+    InstructorName: string;
+    LearningHours: number;
+
+    RegistrationDate: string;
+    Payment: boolean;
+}
+
+const registration_one: CourseRegistration = {
+    StudentId: 1029394,
+    StudentName: "Zafri Ihza Maheswara",
+    GradeLvl: 10,
+
+    CourseId: 10293848,
+    CourseTitle: "Jr. Software Engineering",
+    InstructorName: "Miss Whyna",
+    LearningHours: 2,
+
+    RegistrationDate: "Jum'at, 28 Agustus 2026",
+    Payment: true,
+};
+
+const registration_two: CourseRegistration = {
+    StudentId: 1029395,
+    StudentName: "Alya Putri",
+    GradeLvl: 11,
+    CourseId: 10293849,
+    CourseTitle: "Web Development",
+    InstructorName: "Miss Whyna",
+    LearningHours: 3,
+    RegistrationDate: "Sabtu, 29 Agustus 2026",
+    Payment: false,
+};
+
+const registration_three: CourseRegistration = {
+    StudentId: 1029396,
+    StudentName: "Bima Pratama",
+    GradeLvl: 12,
+    CourseId: 10293850,
+    CourseTitle: "TypeScript Fundamentals",
+    InstructorName: "Miss Whyna",
+    LearningHours: 4,
+    RegistrationDate: "Minggu, 30 Agustus 2026",
+    Payment: true,
+};
+
+console.log(registration_one);
+console.log(registration_two);
+console.log(registration_three);
