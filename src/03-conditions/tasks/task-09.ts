7ccc5ef06efe760147237f5f26a2246c24907565
zafrikeren1 <zafrimaheswara21@gmail.com>
2026-09-13 18:44:56 +0700

    conditions 09-10

--- src/03-conditions/tasks/task-09.ts
+++ src/03-conditions/tasks/task-09.ts
@@ -4,15 +4,14 @@
 Business Rules
 
 If the machine is powered off:
-
 Display Machine Offline.
 
 Otherwise, check its operating condition.
 
 If the machine temperature exceeds 90°C:
-
 If vibration level is High, display:
 Emergency Shutdown
+
 Otherwise:
 Cooling Required
 
@@ -36,3 +35,29 @@ Student Tasks
 - Translate every business rule into conditional statements.
 - Display the final machine status.
  */
+
+const isPoweredOn: boolean = true;
+const temperature: number = 95;
+const isHighVibration: boolean = false;
+const productionSpeed: number = 92;
+
+console.log("Machine Status:");
+
+if (!isPoweredOn) {
+  console.log("Machine Offline");
+} else {
+    if (temperature > 90) {
+        if (isHighVibration) {
+        console.log("Emergency Shutdown");
+        } else {
+        console.log("Cooling Required");
+        }
+    }
+
+
+    if (productionSpeed < 80) {
+        console.log("Performance Warning");
+        } else {
+        console.log("Machine Operating Normally");
+        }
+    }
--- src/03-conditions/tasks/task-10.ts
+++ src/03-conditions/tasks/task-10.ts
@@ -45,3 +45,27 @@ Student Tasks
 - Use nested conditional statements.
 - Display the final registration result.
  */
+
+const studentName: string = "Nadia Putri";
+const isActiveStudent: boolean = true;
+const hasPaidTuition: boolean = true;
+const hasPassedProgrammingFundamentals: boolean = true;
+const hasPassedDatabaseSystems: boolean = true;
+const gpa: number = 3.45;
+const seatsAvailable: boolean = false;
+
+console.log("Student Name:", studentName);
+
+if (!isActiveStudent || !hasPaidTuition) {
+    console.log("Registration Rejected");
+} else {
+    if (!hasPassedProgrammingFundamentals || !hasPassedDatabaseSystems || gpa < 3.20) {
+        console.log("Academic Requirements Not Met");
+    } else {
+        if (seatsAvailable) {
+            console.log("Registration Successful");
+        } else {
+            console.log("Added to Waiting List");
+        }
+    }
+}
